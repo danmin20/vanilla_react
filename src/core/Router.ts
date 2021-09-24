56e8b01dee90d6846ae2164bb5ca92b4cdb44e69
lee jeongmin <danmin20@khu.ac.kr>
2021-09-24 16:57:30 +0900

    feat: routing 구현

---
+++ src/core/Router.ts
@@ -0,0 +1,80 @@
+import Component from './Component';
+
+export type Route = {
+  [key: string]: Component;
+};
+
+const DEFAULT_PATH: string = '/';
+
+export type LinkProps = {
+  to: string;
+};
+
+export default class Link extends Component<LinkProps> {
+  constructor(props: LinkProps) {
+    super(props);
+
+    this.init();
+    this.$element.addEventListener('click', this.onClick.bind(this));
+  }
+
+  render() {
+    return `<div>${this.props.to}</div>`;
+  }
+
+  onClick() {
+    const routeEvent = new CustomEvent('pushstate', {
+      detail: {
+        pathname: this.props.to,
+      },
+    });
+
+    window.dispatchEvent(routeEvent);
+  }
+}
+
+export class Router {
+  $root: HTMLElement;
+  routes: Route;
+  history: string[];
+
+  constructor(routes: Route, $root: HTMLElement) {
+    if (!routes[DEFAULT_PATH])
+      console.error('default directory should be exist');
+
+    this.routes = routes;
+    this.$root = $root;
+    this.history = [];
+
+    this.initEvent();
+    this.handleRoute();
+  }
+
+  initEvent(): void {
+    window.addEventListener('pushstate', (e: any) => {
+      const path = e.detail.pathname;
+      window.history.pushState({}, '', path);
+      this.history.push(path);
+      this.handleRoute();
+    });
+
+    window.addEventListener('popstate', (e: any) => {});
+  }
+
+  getPath(): string {
+    return this.routes[window.location.pathname]
+      ? window.location.pathname
+      : DEFAULT_PATH;
+  }
+
+  getRoute(): HTMLElement {
+    const path = this.getPath();
+    return this.routes[path].$element;
+  }
+
+  handleRoute(): void {
+    const $currRoute: HTMLElement = this.getRoute();
+    this.$root.replaceWith($currRoute);
+    this.$root = $currRoute;
+  }
+}
